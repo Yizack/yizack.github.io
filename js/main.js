d62b45ebac5ea39781b9d1c3f9a59bbc40b6f9c8
Yizack <yizackr@gmail.com>
2018-12-31 01:16:04 -0500

    Web deploy

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
 (function ($) {
 "use strict";
-	/*------------- preloader js --------------*/
-	$(window).on('load',function() { // makes sure the whole site is loaded
-		$('.preloder-wrap').fadeOut(); // will first fade out the loading animation
-		$('.loader').delay(150).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	$(window).on('load',function() {
+		$('.loader').delay(150).fadeOut('slow');
 		$('body').delay(150).css({'overflow':'visible'})
 	})
 
-	/*--
-		Scroll Up
-	------------------------*/
 	$.scrollUp({
 		easingType: 'linear',
 		scrollSpeed: 900,
