e8c870539d3511e48f7730da33cd7a4e27e0140f
Yizack Rangel <yizackr@gmail.com>
2023-06-20 03:22:27 +0200

    chore: updated portfolio

--- utils/portfolio.js
+++ utils/portfolio.js
@@ -85,7 +85,7 @@ export const portfolio = {
     "Time management"
   ],
   skills: {
-    JavaScript: ["ES6+", "Node.js", "Vue.js", "Nuxt.js"],
+    JavaScript: ["ES6+", "Node.js", "Vue.js", "Nuxt"],
     SQL: ["PostgreSQL", "MySQL", "SQLite", "Cloudflare D1", "Drizzle-ORM"],
     PHP: ["Native", "WordPress", "Composer"],
     Cloud: ["Cloudflare", "AWS", "Heroku", "Google Cloud"],
@@ -106,20 +106,21 @@ export const portfolio = {
       url: "https://mynpmlist.yizack.com/",
       github: "https://github.com/Yizack/my-npm-list",
       image: "/images/my-npm-list.jpg",
-      technologies: ["Nuxt.js", "Node.js", "Vue.js", "Javascript", "Cloudflare Pages", "Cloudflare D1", "Github API", "Bootstrap 5", "Drizzle-ORM", "SQLite"]
+      technologies: ["Nuxt", "Node.js", "Vue.js", "Javascript", "Cloudflare Pages", "Cloudflare D1", "Github API", "Bootstrap 5", "Drizzle-ORM", "SQLite"]
     },
     {
       name: "nuxt-twemoji",
       description: "Twemoji module for Nuxt. Rendering emojis as SVG elements or PNG images.",
       period: "May 2023 — Present",
       bullets: [
-        "Developed a Nuxt.js module to render twitter emojis as SVG elements or PNG images.",
-        "Published the module on NPM"
+        "Developed a Nuxt module with multiple components to render twitter emojis as SVG elements or PNG images.",
+        "Published the module on the NPM registry.",
+        "Submitted the module to the Nuxt community modules list."
       ],
       url: "https://github.com/Yizack/nuxt-twemoji",
       github: "https://github.com/Yizack/nuxt-twemoji",
       image: "/images/nuxt-twemoji.png",
-      technologies: ["Nuxt.js", "Node.js", "Typescript", "Javascript", "GitHub Actions", "Vue.js", "Twemoji", "NPM"]
+      technologies: ["Nuxt", "Node.js", "Typescript", "Javascript", "GitHub Actions", "Vue.js", "Twemoji", "NPM"]
     },
     {
       name: "How Many Times Larger",
@@ -128,14 +129,14 @@ export const portfolio = {
       bullets: [
         "Developed a Web App project on Github Pages used to compare the size of countries using their total area.",
         "Automatically downloaded most of the images of the countries' flags and shapes using Node.js.",
-        "Designed and implemented a game to test the visitor's knowledge of the countries, using Vue.js and Nuxt.js.",
+        "Designed and implemented a game to test the visitor's knowledge of the countries, using Vue.js and Nuxt.",
         "Created an internal API class.",
         "Bundled an Android app through Github Actions using Capacitor.js and published it on Google Play Store."
       ],
       url: "https://howmanytimeslarger.com/",
       image: "/images/howmanytimeslarger.jpg",
       github: "https://github.com/Yizack/HowManyTimesLarger",
-      technologies: ["Vue.js", "Nuxt.js", "Node.js", "Capacitor.js", "GitHub Pages", "GitHub Actions", "Bootstrap 5"]
+      technologies: ["Vue.js", "Nuxt", "Node.js", "Capacitor.js", "GitHub Pages", "GitHub Actions", "Bootstrap 5"]
     },
     {
       name: "Devago",
@@ -185,8 +186,8 @@ export const portfolio = {
       period: "Sep 2021 — Present",
       description: "The PESP.GG website is a collaborative project between programmers, graphic designers and the company Panama Esports Productions for the creation of the company's official website",
       bullets: [
-        "The project was initially developed using PHP hosted on AWS EC2, then migrated to Nuxt.js hosted on Cloudflare Pages.",
-        "Developed an Esports website project using Nuxt.js hosted on Cloudflare Pages.",
+        "The project was initially developed using PHP hosted on AWS EC2, then migrated to Nuxt hosted on Cloudflare Pages.",
+        "Developed an Esports website project using Nuxt hosted on Cloudflare Pages.",
         "Implemented an admin dashboard to manage the website's posts and see statistics.",
         "Designed a Database for the website's posts using SQLite in development and Cloudflare D1 in production using Drizzle-ORM for SQL queries.",
         "Implemented an images and posts html content storage system using Cloudflare R2."
@@ -194,7 +195,7 @@ export const portfolio = {
       url: "https://pesp.gg/",
       image: "/images/pespgg.jpg",
       github: "https://github.com/Yizack/pesp.gg",
-      technologies: ["PHP", "Bootstrap 5", "MySQL", "AWS EC2", "Bootstap 4", "Slack", "JQuery", "Cloudflare D1", "Cloudflare R2", "Nuxt.js", "Vue.js"]
+      technologies: ["PHP", "Bootstrap 5", "MySQL", "AWS EC2", "Bootstap 4", "Slack", "JQuery", "Cloudflare D1", "Cloudflare R2", "Nuxt", "Vue.js"]
     },
     {
       name: "Panama Metrobus Balance",
@@ -217,14 +218,14 @@ export const portfolio = {
       period: "Dec 2018 — Present",
       description: "Music creator website.",
       bullets: [
-        "Built a music creator website to showcase all the artist's albums and songs, biography, licensing, and links to music platforms and stores. Using Nuxt.js hosted on GitHub Pages.",
+        "Built a music creator website to showcase all the artist's albums and songs, biography, licensing, and links to music platforms and stores. Using Nuxt hosted on GitHub Pages.",
         "Created a beautiful HTML/CSS music player to play audio.",
         "Deployed serverless code on Cloudflare Workers while using the Meta API to show the Instagram feed and the Spotify API to show the followers on the website."
       ],
       url: "https://dimatis.yizack.com/",
       image: "/images/dimatis.jpg",
       github: "https://github.com/Yizack/dimatis",
-      technologies: ["Nuxt.js", "Node.js", "Cloudflare Workers", "Bootstrap 5", "GitHub Pages", "Github Actions", "Spotify API", "Meta API"]
+      technologies: ["Nuxt", "Node.js", "Cloudflare Workers", "Bootstrap 5", "GitHub Pages", "Github Actions", "Spotify API", "Meta API"]
     },
     {
       name: "Spotify Playlist Adder",
