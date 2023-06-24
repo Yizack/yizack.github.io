5e203e0658f5ac8b52be8a7b567e884808c5baab
Yizack Rangel <yizackr@gmail.com>
2023-06-24 15:05:15 +0200

    chore: fix web

--- error.vue
+++ error.vue
@@ -1,16 +1,10 @@
+<script setup>
+import "~/assets/css/not-found.css";
+</script>
+
 <template>
   <div class="cover">
     <h1>Page Not Found <small>Error 404</small></h1>
     <p class="lead">Sorry, an error has occured, Requested page not found.</p>
   </div>
 </template>
-
-<style>
-@import "~/assets/css/not-found.css";
-</style>
-
-<script>
-export default {
-  name: "NotFound"
-};
-</script>
