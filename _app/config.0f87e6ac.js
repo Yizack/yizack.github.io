6014650ce6582a3290a283dae75ec04cee4b5282
Yizack <yizackr@gmail.com>
2023-10-29 21:46:46 +0000

    Deploying to gh-pages from @ Yizack/yizack.github.io@221bddc5df6b0b534787b1afeb88ac3629384b71 🚀

--- _app/config.0f87e6ac.js
+++
@@ -1,1 +0,0 @@
-import{W as p,X as o,K as i}from"./entry.8c890e97.js";const t={nuxt:{}},f=p(t);function s(){const n=i();return n._appConfig||(n._appConfig=o(f)),n._appConfig}export{s as u};
---
+++ _app/config.11e71815.js
@@ -0,0 +1,1 @@
+import{W as p,X as o,K as i}from"./entry.7c70d610.js";const t={nuxt:{}},f=p(t);function s(){const n=i();return n._appConfig||(n._appConfig=o(f)),n._appConfig}export{s as u};
