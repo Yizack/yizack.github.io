cb1b5c467e5cb9d93394ecd5cfbe60c98471121f
Yizack Rangel <yizackr@gmail.com>
2024-08-02 12:18:04 -0500

    chore: update error page

--- error.vue
+++ error.vue
@@ -1,12 +1,16 @@
-<script setup>
-const { statusCode, message } = useError().value;
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+defineProps({
+  error: { type: Object as () => NuxtError, required: true }
+});
 </script>
 
 <template>
   <div class="container vh-100 d-flex justify-content-center align-items-center">
     <div>
-      <h1>Error {{ statusCode }}</h1>
-      <h5 class="text-body">Sorry, an error has occured. {{ message }}</h5>
+      <h1>Error {{ error.statusCode }}</h1>
+      <h5 class="text-body">Sorry, an error has occured. {{ error.statusMessage || error.message || "error" }}</h5>
     </div>
   </div>
 </template>
