0882ac92f45c8aaf8224897ac174c680ee7143a7
Yizack Rangel <yizackr@gmail.com>
2025-02-13 14:32:09 -0500

    chore: hover controls

--- app/components/ImageCarousel.vue
+++ app/components/ImageCarousel.vue
@@ -25,13 +25,13 @@ const goTo = (index: number) => {
         <div class="flex transition-transform duration-500" :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
           <div v-for="(image, index) in images" :key="index" class="w-full flex-shrink-0">
             <img :src="image.src" class="w-full h-full object-cover" :alt="image.alt" :title="image.alt">
-            <div v-if="image.alt" class="absolute bottom-0 w-full text-center py-1 bg-slate-700 bg-opacity-75 hidden group-hover:block">
+            <div v-if="image.alt" class="absolute bottom-0 w-full text-center py-1 bg-slate-200 dark:bg-slate-700 bg-opacity-75 dark:bg-opacity-75 hidden group-hover:block">
               <span>{{ image.alt }}</span>
             </div>
           </div>
         </div>
       </div>
-      <div v-if="images.length > 1" class="absolute top-1/2 transform -translate-y-1/2 flex justify-between w-full px-4">
+      <div v-if="images.length > 1" class="absolute top-1/2 transform -translate-y-1/2 justify-between w-full px-4 hidden group-hover:flex">
         <button class="bg-gray-800 text-white p-2 rounded-md hover:bg-gray-700 bg-opacity-50" @click="prev">
           <Icon name="tabler:chevron-left" />
         </button>
@@ -40,12 +40,12 @@ const goTo = (index: number) => {
         </button>
       </div>
     </div>
-    <div v-if="images.length > 1" class="flex justify-center py-1 bg-slate-700 relative z-10">
+    <div v-if="images.length > 1" class="flex justify-center py-2 bg-slate-200 dark:bg-slate-700 relative z-10">
       <span
         v-for="(image, index) in images"
         :key="index"
         class="h-2 w-2 rounded-full mx-1 cursor-pointer"
-        :class="currentIndex === index ? 'bg-slate-200' : 'bg-gray-500'"
+        :class="currentIndex === index ? 'dark:bg-slate-100 bg-slate-800' : 'dark:bg-gray-500 bg-gray-400'"
         :title="image.alt"
         @click="goTo(index)"
       />
