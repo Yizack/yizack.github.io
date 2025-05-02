9fd32f175fe4dc1fbb1092bbb796e06fa20bc19b
Yizack Rangel <yizackr@gmail.com>
2025-05-01 20:24:38 -0500

    chore: adjust error page

--- app/error.vue
+++ app/error.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import type { NuxtError } from "#app";
 
-defineProps({
-  error: { type: Object as () => NuxtError, required: true }
-});
+defineProps<{
+  error: NuxtError;
+}>();
 </script>
 
 <template>
   <div class="flex items-center justify-center h-screen dark:bg-gray-900 dark:text-white bg-white text-gray-900">
     <div class="rounded-md dark:bg-slate-800 bg-slate-200 p-10 shadow-lg">
       <h1 class="text-3xl font-bold">Error {{ error.statusCode }}</h1>
-      <h5 class="dark:text-gray-300 text-gray-700">Sorry, an error has occured. {{ error.statusMessage || error.message || "error" }}</h5>
+      <h5 class="dark:text-gray-300 text-gray-700">Sorry, an error has occured. {{ error.message || error.statusMessage || "error" }}</h5>
       <span>Go back: <NuxtLink to="/" class="underline">{{ SITE.domain }}</NuxtLink></span>
     </div>
   </div>
