3fc39b61831e961b712fc8212f71e94c353b3fc0
Yizack Rangel <yizackr@gmail.com>
2025-08-26 21:23:06 +0200

    chore: fix typo

--- app/error.vue
+++ app/error.vue
@@ -10,7 +10,7 @@ defineProps<{
   <div class="flex items-center justify-center h-screen dark:bg-gray-900 dark:text-white bg-white text-gray-900">
     <div class="rounded-md dark:bg-slate-800 bg-slate-200 p-10 shadow-lg">
       <h1 class="text-3xl font-bold">Error {{ error.statusCode }}</h1>
-      <h5 class="dark:text-gray-300 text-gray-700">Sorry, an error has occured. {{ error.message || error.statusMessage || "error" }}</h5>
+      <h5 class="dark:text-gray-300 text-gray-700">Sorry, an error has occurred. {{ error.message || error.statusMessage || "error" }}</h5>
       <span>Go back: <NuxtLink to="/" class="underline">{{ SITE.domain }}</NuxtLink></span>
     </div>
   </div>
