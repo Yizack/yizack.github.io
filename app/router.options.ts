7a2995d889a2213df4eb610eff0ebb3be0bb4ab7
Yizack Rangel <yizackr@gmail.com>
2026-08-22 22:09:43 +0200

    fix(router): enable hash scrolling with 80px offset

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,7 +1,7 @@
 import type { RouterConfig } from "@nuxt/schema";
 
-export default <RouterConfig>{
+export default {
   scrollBehavior (to) {
-    if (!to.hash) return { left: 0, top: 0 };
+    if (to.hash) return { el: to.hash, top: 80 };
   }
-};
+} satisfies RouterConfig;
